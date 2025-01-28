acb97ad95a7c914ad923d07d048a185b342c1c41
Nikita I <vt231_ini@student.ztu.edu.ua>
2025-01-28 10:50:46 +0200

    refractor - splitin a large clickMe function into smaller functions

--- script.js
+++ script.js
@@ -12,16 +12,28 @@ const TX = 0.15;//fix added
 function calculatePrice(price) {
   return price * TX;
 }
-//fix added
 
-document.getElementById("clickMe").addEventListener("click",() => 
-{
+////////////////////////////////////////////////////
+
+function updateOutputMessage() {
   const output = document.getElementById("output");
-  output.innerHTML = "Button clicked!";
+  output.innerHTML = "Button clicked";
+}
+
+function logIterations() {
   for (let i = 0; i<5; i++) {
     console.log("Iteration " +i);
   }
+}
+
+function calculateAndLogSum() {
   const result =sum(10,20);
   console.log("Sum result: " + result);
+}
+
+document.getElementById("clickMe").addEventListener("click",() => {
+  updateOutputMessage();
+  logIterations();
+  calculateAndLogSum();
 });
 
