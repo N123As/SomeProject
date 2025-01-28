7afa082d4b1b9b912fff4fdfca9e04460f51e223
Nikita I <vt231_ini@student.ztu.edu.ua>
2025-01-28 10:37:37 +0200

    go

---
+++ script.js
@@ -0,0 +1,30 @@
+function showMessage1() {
+  const output = document.getElementById("output");
+  output.innerHTML = "Button was clicked";
+}
+
+function showMessage2() {
+  const output = document.getElementById("output");
+  output.innerHTML = "Button was clicked";
+}
+
+function calculatePrice(price) {
+  return price * 0.15;
+}
+
+function sum(a, b) {
+   const x = a + b;//no way X
+  return x;
+}
+
+document.getElementById("clickMe").addEventListener("click",() => 
+{
+  const output = document.getElementById("output");
+  output.innerHTML = "Button clicked!";
+  for (let i = 0; i<5; i++) {
+    console.log("Iteration " +i);
+  }
+  const result =sum(10,20);
+  console.log("Sum result: " + result);
+});
+
